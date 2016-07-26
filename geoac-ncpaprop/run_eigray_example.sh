23f5bae8d705fbb377a27452bb09db3812fdcfe1
Claus Hetzer <claus@olemiss.edu>
2016-07-26 10:12:33 -0700

    Incorporated GeoAc-ncpaprop into distribution

---
+++ geoac-ncpaprop/run_eigray_example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../GeoAc3D-ncpaprop -eig_search ../NCPA_canonical_profile_zuvwtdp.dat theta_min=2 theta_max=10 bnc_min=0 bnc_max=1 z_src=0 x_rcvr=189 y_rcvr=189 Verbose='false' profile_format='zuvwTdp'
